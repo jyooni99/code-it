8d6a5b54f846bcdc32f4fc24b8dadb247378a918
jyooni <berry3891@naver.com>
2025-09-08 18:13:49 +0900

    motion 기초

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { motion } from "motion/react"; // use client 추가해야 함
+// import * as motion from "motion/react-client"; -> 서버 컴포넌트에서 animation 사용 가능
+
 export default function Home() {
   return (
-    <div className="animate-show flex h-screen items-center justify-center gap-2">
-      <div className="h-96 w-72 rounded-lg border shadow-md"></div>
-    </div>
+    <motion.div
+      initial={{ translateX: -100, opacity: 0 }} // 초기 값
+      animate={{ translateX: 0, opacity: 1 }} // 애니메이션
+      transition={{ duration: 0.8 }} // 애니메이션 지속 시간
+    >
+      왼쪽에서 슬라이드 인!
+    </motion.div>
   );
 }
