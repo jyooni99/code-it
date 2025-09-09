a99daa681a4260ed43eba747aba8efbf70b8d068
jyooni <berry3891@naver.com>
2025-09-09 11:35:21 +0900

    react-intersection-observer를 활용한 무한스크롤 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,52 @@
 "use client";
 
-import * as React from "react";
-import { motion, useScroll, useTransform } from "motion/react";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
-const PAGE_COUNT = 5;
+export default function Home() {
+  const { ref, inView } = useInView({ threshold: 1 });
 
-function ScrollLinked() {
-  const { scrollYProgress } = useScroll(); // useScroll: 현재 스크롤 위치 감지
-  const clipPath = useTransform(
-    scrollYProgress,
-    (scrollYProgress) => `circle(${scrollYProgress * 100}%)`, // 스크롤 위치 기반으로 원 크기 조절
-  );
+  const [items, setItems] = useState([...Array(10)].map((_, i) => i + 1));
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadMoreItems = async () => {
+    setIsLoading(true);
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    const nextPage = page + 1;
+    const newItems = [...Array(10)].map((_, i) => items.length + i + 1);
+
+    setItems((prevItems) => [...prevItems, ...newItems]);
+    setPage(nextPage);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (inView) {
+      loadMoreItems();
+    }
+  }, [inView]);
 
   return (
-    <div className="h-full w-full bg-gray-900">
-      <div className="fixed inset-0">
-        <motion.div
-          className="absolute top-1/2 left-1/2 flex h-full w-full -translate-x-1/2 -translate-y-1/2 items-center justify-center bg-orange-500"
-          style={{ clipPath }}
-        >
-          <div className="text-center">
-            <h1 className="flex flex-col gap-4 text-8xl font-bold text-blue-600">
-              <span>
-                <span>Aha!</span>
-              </span>
-              <span>
-                <span>You found me!</span>
-              </span>
-            </h1>
+    <div className="container mx-auto">
+      <div className="flex flex-col gap-4">
+        {items.map((item) => (
+          <div className="h-48 border border-gray-500" key={item}>
+            {item}
           </div>
-        </motion.div>
+        ))}
       </div>
-
-      {new Array(PAGE_COUNT).fill(null).map((_, index) => (
-        <div className="h-screen w-screen" key={index} />
-      ))}
+      {/* 해당 div가 뷰포트에 다 보일 때 더 로드 */}
+      <div className="py-4 text-center" ref={ref}>
+        <div className="flex items-center justify-center space-x-2">
+          <div className="h-4 w-4 animate-pulse rounded-full bg-blue-500"></div>
+          <div className="h-4 w-4 animate-pulse rounded-full bg-blue-500"></div>
+          <div className="h-4 w-4 animate-pulse rounded-full bg-blue-500"></div>
+          <span className="text-gray-500">로딩 중...</span>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default ScrollLinked;
