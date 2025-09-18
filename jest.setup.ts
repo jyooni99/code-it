5e8a96c00d73a6560bf1bec76de883650af267ba
jyooni <berry3891@naver.com>
2025-09-18 13:53:56 +0900

    import 경로 수정

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,7 +5,7 @@
 // 3. 일관성 유지: 모든 테스트에 동일한 확장 기능이 적용되므로 테스트 코드가 일관성을 유지합니다.
 // 4. 설정 집중화: 테스트 환경 설정을 한 곳에서 관리할 수 있어 나중에 변경이 필요할 때 편리합니다.
 import "@testing-library/jest-dom";
-import { server } from "@/src/mocks";
+import { server } from "@/src/mocks/server";
 
 // 모든 테스트가 시작하기 전 MSW 서버를 시작합니다.
 beforeAll(() => server.listen());
